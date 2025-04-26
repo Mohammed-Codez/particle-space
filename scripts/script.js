a4e2897e3ba749b0c3622b3e0521cce488a84558
Mohammed-Codez <amakactive@gmail.com>
2025-04-26 23:05:46 +0400

    star

--- scripts/script.js
+++ scripts/script.js
@@ -193,8 +193,8 @@ ogAtoms.push(...[...Array(500)].map(() => {
         let randRad = Math.random() * maxRad
         let circPos = Math.random() * 2 * Math.PI
 
-        let randX = Math.sin(circPos) * (Math.sin(5 * circPos) + 5) / 6
-        let randY = Math.cos(circPos) * (Math.sin(5 * circPos) + 5) / 6
+        let randX = Math.sin(circPos) * (Math.sin(5 * circPos) + 5) / 6 
+        let randY = Math.cos(circPos) * (Math.sin(5 * circPos) + 5) / 6 
 
         return new Atom(
                 randX,
