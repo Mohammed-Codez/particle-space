8cfa899e827133cf7622071139140cf01c7e0a26
Mohammed-Codez <amakactive@gmail.com>
2025-04-28 15:43:38 +0400

    ring

--- scripts/script.js
+++ scripts/script.js
@@ -209,7 +209,7 @@ ogAtoms.push(...[...Array(500)].map(() => {
 }))
 
 // ring 1
-ogAtoms.push(...[...Array(100)].map(() => {
+ogAtoms.push(...[...Array(250)].map(() => {
         let maxRad = 100
         let randRad = Math.random() * 25 + maxRad
         let circPos = Math.random() * 2 * Math.PI
