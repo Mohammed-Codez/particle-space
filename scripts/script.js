8116e0f1c80ae20a175c2df851b6c944760f6bff
Mohammed-Codez <amakactive@gmail.com>
2025-04-28 22:24:56 +0400

    will merge together

--- scripts/script.js
+++ scripts/script.js
@@ -210,9 +210,9 @@ ogAtoms.push(...[...Array(500)].map(() => {
 
 // ring 1
 ogAtoms.push(...[...Array(240)].map(() => {
-        let maxRad = 250
+        let maxRad = 350
         let randRad = Math.random() * 25 + maxRad
-        let circPos = Math.random() * 2 * Math.PI
+        let circPos = Math.random() * 20 * Math.PI
 
         let randX = Math.sin(circPos) * randRad
         let randY = Math.cos(circPos) * randRad
@@ -229,9 +229,9 @@ ogAtoms.push(...[...Array(240)].map(() => {
 
 // ring 2
 ogAtoms.push(...[...Array(240)].map(() => {
-        let maxRad = 300
+        let maxRad = 550
         let randRad = Math.random() * 25 + maxRad
-        let circPos = Math.random() * 2 * Math.PI
+        let circPos = Math.random() * 20 * Math.PI
 
         let randX = Math.sin(circPos) * randRad
         let randY = Math.cos(circPos) * randRad
