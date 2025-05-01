a5ca9cc8359a4eb31f781b2915faaf8f7023cbf1
Mohammed-Codez <amakactive@gmail.com>
2025-05-01 13:02:44 +0400

    it explode

--- scripts/script.js
+++ scripts/script.js
@@ -273,7 +273,7 @@ ogAtoms.push(...[...Array(200)].map(() => {
 
 // moon
 ogAtoms.push(...[...Array(100)].map(() => {
-        let maxRad = 15
+        let maxRad = 17.5
         let randRad = Math.random() * maxRad
         let circPos = Math.random() * 2 * Math.PI
 
